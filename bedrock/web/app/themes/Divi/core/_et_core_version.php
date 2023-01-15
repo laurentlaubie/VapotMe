d4e3e4bd51f0bd72745cb5076806d6937d7d6b89
Laurent <laurent.laubie31@gmail.com>
2023-01-15 18:52:02 +0100

    add new theme and remove old theme

---
+++ bedrock/web/app/themes/Divi/core/_et_core_version.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * Define the current ET Core version4.19.4
+ *
+ * (Note: this value updates automatically as part of our Grunt release task4.19.4)
+ *
+ * @package \ET\Core
+ */
+
+// Note, this will be updated automatically during grunt release task
+$ET_CORE_VERSION = '4.19.4';
